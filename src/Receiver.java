cb54b27d2bf9960b1a6e55e751be7c3099299270
Samuel Dufresne <samuel.dufresne@live.com>
2017-12-01 02:59:58 -0500

    Working version
    Requires some more refactoring

--- src/Receiver.java
+++ src/Receiver.java
@@ -10,100 +10,112 @@ import java.util.zip.CRC32;
 
 public class Receiver {
 	static int pkt_size = 1000;
-	
+	String message;
+
+	public String getMessage() {
+		return message;
+	}
+
 	// Receiver constructor
-	public Receiver(int senderPort, int receiverPort, String message) {
+	public Receiver(int senderPort, int receiverPort) {
 		DatagramSocket senderSocket, receiverSocket;
 		System.out.println("Receiver: sender_port=" + senderPort + ", " + "receiver_port=" + receiverPort + ".");
-		
-		int prevSeqNum = -1;				// previous sequence number received in-order 
-		int nextSeqNum = 0;					// next expected sequence number
-		boolean isTransferComplete = false;	// (flag) if transfer is complete
-		
+
+		int prevSeqNum = -1; // previous sequence number received in-order
+		int nextSeqNum = 0; // next expected sequence number
+		boolean isTransferComplete = false; // (flag) if transfer is complete
+
 		// create sockets
 		try {
 			senderSocket = new DatagramSocket(senderPort);
 			receiverSocket = new DatagramSocket();
 			System.out.println("Receiver: Listening");
 			try {
-				byte[] in_data = new byte[pkt_size];									// message data in packet
-				DatagramPacket in_pkt = new DatagramPacket(in_data,	in_data.length);	// incoming packet
+				byte[] in_data = new byte[pkt_size]; // message data in packet
+				DatagramPacket in_pkt = new DatagramPacket(in_data, in_data.length); // incoming packet
 				InetAddress senderAddress = InetAddress.getByName("127.0.0.1");
-				
+
 				FileOutputStream fos = null;
 				// make directory
-//				path = ((path.substring(path.length()-1)).equals("/"))? path: path + "/";	// append slash if missing
-//				File filePath = new File(path);
-//				if (!filePath.exists()) filePath.mkdir();
-				
+				// path = ((path.substring(path.length()-1)).equals("/"))? path: path + "/"; //
+				// append slash if missing
+				// File filePath = new File(path);
+				// if (!filePath.exists()) filePath.mkdir();
+
 				// listen on sk2_dst_port
 				while (!isTransferComplete) {
 					// receive packet
 					senderSocket.receive(in_pkt);
+					String output = new String(in_pkt.getData());
 
 					byte[] received_checksum = copyOfRange(in_data, 0, 8);
-					// cyclic-redundancy-check
 					CRC32 checksum = new CRC32();
 					checksum.update(copyOfRange(in_data, 8, in_pkt.getLength()));
 					byte[] calculated_checksum = ByteBuffer.allocate(8).putLong(checksum.getValue()).array();
-					
+
 					// if packet is not corrupted
-					if (Arrays.equals(received_checksum, calculated_checksum)){
+					if (Arrays.equals(received_checksum, calculated_checksum)) {
 						int seqNum = ByteBuffer.wrap(copyOfRange(in_data, 8, 12)).getInt();
 						System.out.println("Receiver: Received sequence number: " + seqNum);
-						
+
 						// if packet received in order
-						if (seqNum == nextSeqNum){
-							// if final packet (no data), send teardown ack
-							if (in_pkt.getLength() == 12){
-								byte[] ackPkt = generatePacket(-2);	// construct teardown packet (ack -2)
-								// send 20 acks in case last ack is not received by Sender (assures Sender teardown)
-								for (int i=0; i<20; i++) receiverSocket.send(new DatagramPacket(ackPkt, ackPkt.length, senderAddress, receiverPort));
-								isTransferComplete = true;			// set flag to true
-								System.out.println("Receiver: All packets received! File Created!");
-								continue;	// end listener
+						if (seqNum == nextSeqNum) {
+							// no data packet = 28 bytes
+							if (in_pkt.getLength() == 28) {
+								byte[] ackPkt = generatePacket(-2); // construct teardown packet (ack -2)
+								// send 20 acks in case last ack is not received by Sender (assures Sender
+								// teardown)
+								for (int i = 0; i < 20; i++) {
+									receiverSocket
+											.send(new DatagramPacket(ackPkt, ackPkt.length, senderAddress, senderPort));
+								}
+								isTransferComplete = true;
+								System.out.println("Receiver: All packets received!");
+								continue; // end listener
 							}
-							// else send ack
-							else{
+							// send ack that a packet was received
+							else {
 								byte[] ackPkt = generatePacket(seqNum);
-								receiverSocket.send(new DatagramPacket(ackPkt, ackPkt.length, senderAddress, receiverPort));
+								receiverSocket
+										.send(new DatagramPacket(ackPkt, ackPkt.length, senderAddress, receiverPort));
 								System.out.println("Receiver: Sent Ack " + seqNum);
 							}
-							
+
 							// if first packet of transfer
-							if (seqNum==0 && prevSeqNum==-1){
-								int fileNameLength = ByteBuffer.wrap(copyOfRange(in_data, 12, 16)).getInt();	// 0-8:checksum, 8-12:seqnum
-								String fileName = new String(copyOfRange(in_data, 16, 16 + fileNameLength));	// decode file name
-								System.out.println("Receiver: fileName length: " + fileNameLength + ", fileName:" + fileName);
-								
-								
-								//TODO: replace this code with taking a string
-								// create file
-//								File file = new File(path + fileName);
-//								if (!file.exists()) file.createNewFile();
-								// init fos
-//								fos = new FileOutputStream(file);
-//								// write initial data to fos
-//								fos.write(in_data, 16 + fileNameLength, in_pkt.getLength() - 16 - fileNameLength);
+							if (seqNum == 0 && prevSeqNum == -1) {
+								// Goes here the first time
+								int messageLength = ByteBuffer.wrap(copyOfRange(in_data, 12, 16)).getInt(); // 0-8:checksum,
+																											// 8-12:seqnum
+								message = new String(copyOfRange(in_data, 16, 16 + messageLength)); // decode file name
+								this.message = message;
+								System.out.println(
+										"Receiver: fileName length: " + messageLength + ", message:" + message);
+								isTransferComplete = true;
+								// Close it right away
+								System.out.println("Receiver: All packets received!");
+								continue; // end listener
 							}
-							
+
 							// else if not first packet write to FileOutputStream
-							else fos.write(in_data, 12, in_pkt.getLength() - 12);
-							
-							nextSeqNum ++; 			// update nextSeqNum
-							prevSeqNum = seqNum;	// update prevSeqNum
+							else {
+								String newPkt = new String(in_pkt.getData());
+								message += newPkt;
+							}
+
+							nextSeqNum++; // update nextSeqNum
+							prevSeqNum = seqNum; // update prevSeqNum
 						}
-						
+
 						// if out of order packet received, send duplicate ack
-						else{
+						else {
 							byte[] ackPkt = generatePacket(prevSeqNum);
 							receiverSocket.send(new DatagramPacket(ackPkt, ackPkt.length, senderAddress, receiverPort));
 							System.out.println("Receiver: Sent duplicate Ack " + prevSeqNum);
 						}
 					}
-					
+
 					// else packet is corrupted
-					else{
+					else {
 						System.out.println("Receiver: Corrupt packet dropped");
 						byte[] ackPkt = generatePacket(prevSeqNum);
 						receiverSocket.send(new DatagramPacket(ackPkt, ackPkt.length, senderAddress, receiverPort));
@@ -123,9 +135,9 @@ public class Receiver {
 			e1.printStackTrace();
 		}
 	}// END constructor
-	
+
 	// generate Ack packet
-	public byte[] generatePacket(int ackNum){
+	public byte[] generatePacket(int ackNum) {
 		byte[] ackNumBytes = ByteBuffer.allocate(4).putInt(ackNum).array();
 		// calculate checksum
 		CRC32 checksum = new CRC32();
@@ -136,22 +148,21 @@ public class Receiver {
 		pktBuf.put(ackNumBytes);
 		return pktBuf.array();
 	}
-	
+
 	// same as Arrays.copyOfRange in 1.6
-	public byte[] copyOfRange(byte[] srcArr, int start, int end){
-		int length = (end > srcArr.length)? srcArr.length-start: end-start;
+	public byte[] copyOfRange(byte[] srcArr, int start, int end) {
+		int length = (end > srcArr.length) ? srcArr.length - start : end - start;
 		byte[] destArr = new byte[length];
 		System.arraycopy(srcArr, start, destArr, 0, length);
 		return destArr;
 	}
-	
+
 	// main function
 	public static void main(String[] args) {
-		// parse parameters
-		if (args.length != 3) {
-			System.err.println("Usage: java Receiver sk2_dst_port, sk3_dst_port, outputFolderPath");
-			System.exit(-1);
-		}
-		else new Receiver(Integer.parseInt(args[0]), Integer.parseInt(args[1]), args[2]);
+		// Ex: ports 4001 4002
+		// follow same sequence for the sender constructor
+		Receiver r = new Receiver(4001, 4002);
+		// Get the message as such
+		System.out.println(r.getMessage());
 	}
 }
--- src/Sender.java
+++ src/Sender.java
@@ -13,52 +13,77 @@ import java.util.zip.CRC32;
 
 // The following implementation uses the Go-Back-N protocol
 public class Sender {
-	static int data_size = 988;			// (checksum:8, seqNum:4, data<=988) Bytes : 1000 Bytes total
+	static int data_size = 988; // (checksum:8, seqNum:4, data<=988) Bytes : 1000 Bytes total
 	static int win_size = 10;
-	static int timeoutVal = 300;		// 300ms until timeout
+	static int timeoutVal = 300; // 300ms until timeout
 
-	int base;					// base sequence number of window
-	int nextSeqNum;				// next sequence number in window
-	String path;				// path of file to be sent
-	String fileName;			// filename to be saved by receiver
-	Vector<byte[]> packetsList;	// list of generated packets
-	Timer timer;				// for timeouts	
-	Semaphore s;				// guard CS for base, nextSeqNum
-	boolean isTransferComplete;	// if receiver has completely received the file
-	
-	// to start or stop the timer
-	public void setTimer(boolean isNewTimer){
-		if (timer != null) timer.cancel();
-		if (isNewTimer){
-			timer = new Timer();
-			timer.schedule(new Timeout(), timeoutVal);
+	int base;
+	int nextSeqNum;
+	String message;
+	Vector<byte[]> packetsList;
+	Timer timer;
+	Semaphore s;
+	boolean isTransferComplete;
+
+	public Sender(int ackPort, int broadPort, String message) {
+		base = 0;
+		nextSeqNum = 0;
+		this.message = message;
+		packetsList = new Vector<byte[]>(win_size);
+		isTransferComplete = false;
+		DatagramSocket ackSocket, broadSocket;
+		s = new Semaphore(1);
+		System.out.println(
+				"Sender: Aknowledgement port=" + ackPort + ", Receiver port=" + broadPort + ", message=" + message);
+
+		try {
+			// create sockets
+			ackSocket = new DatagramSocket(); // outgoing channel
+			broadSocket = new DatagramSocket(broadPort); // incoming channel
+
+			// create threads to process data
+			AcknowledgeThread ackThread = new AcknowledgeThread(broadSocket);
+			BroadcastThread broadThread = new BroadcastThread(ackSocket, ackPort, broadPort);
+			ackThread.start();
+			broadThread.start();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.exit(-1);
 		}
+	}// END Sender constructor
+
+	// same as Arrays.copyOfRange in 1.6
+	public byte[] copyOfRange(byte[] srcArr, int start, int end) {
+		int length = (end > srcArr.length) ? srcArr.length - start : end - start;
+		byte[] destArr = new byte[length];
+		System.arraycopy(srcArr, start, destArr, 0, length);
+		return destArr;
 	}
-	
-	// CLASS OutThread
-	public class OutThread extends Thread {
+
+
+	public class BroadcastThread extends Thread {
 		private DatagramSocket sk_out;
 		private int dst_port;
 		private InetAddress dst_addr;
 		private int recv_port;
 
-		// OutThread constructor
-		public OutThread(DatagramSocket sk_out, int dst_port, int recv_port) {
+		public BroadcastThread(DatagramSocket sk_out, int dst_port, int recv_port) {
 			this.sk_out = sk_out;
 			this.dst_port = dst_port;
 			this.recv_port = recv_port;
 		}
-		
+
 		// constructs the packet prepended with header information
-		public byte[] generatePacket(int seqNum, byte[] dataBytes){
-			byte[] seqNumBytes = ByteBuffer.allocate(4).putInt(seqNum).array(); 				// Seq num (4 bytes)
-			
-			// generate checksum 
+		public byte[] generatePacket(int seqNum, byte[] dataBytes) {
+			byte[] seqNumBytes = ByteBuffer.allocate(4).putInt(seqNum).array(); // Seq num (4 bytes)
+
+			// generate checksum
 			CRC32 checksum = new CRC32();
 			checksum.update(seqNumBytes);
 			checksum.update(dataBytes);
-			byte[] checksumBytes = ByteBuffer.allocate(8).putLong(checksum.getValue()).array();	// checksum (8 bytes)
-			
+			byte[] checksumBytes = ByteBuffer.allocate(8).putLong(checksum.getValue()).array(); // checksum (8 bytes)
+
 			// generate packet
 			ByteBuffer pktBuf = ByteBuffer.allocate(8 + 4 + dataBytes.length);
 			pktBuf.put(checksumBytes);
@@ -68,77 +93,62 @@ public class Sender {
 		}
 
 		// sending process (updates nextSeqNum)
-		public void run(){
-			try{
-				 dst_addr = InetAddress.getByName("127.0.0.1"); // resolve dst_addr
-				// create byte stream
-				FileInputStream fis = new FileInputStream(new File(path));
-				 
+		public void run() {
+			try {
+				dst_addr = InetAddress.getByName("127.0.0.1");
+
 				try {
 					// while there are still packets yet to be received by receiver
-					while (!isTransferComplete){
+					while (!isTransferComplete) {
 						// send packets if window is not yet full
-						if (nextSeqNum < base + win_size){
-							
-							s.acquire();	/***** enter CS *****/
-							if (base == nextSeqNum) setTimer(true);	// if first packet of window, start timer
-							
+						if (nextSeqNum < base + win_size) {
+
+							s.acquire(); /***** enter CS *****/
+
 							byte[] out_data = new byte[10];
 							boolean isFinalSeqNum = false;
-							
+
 							// if packet is in packetsList, retrieve from list
-							if (nextSeqNum < packetsList.size()){
+							if (nextSeqNum < packetsList.size()) {
 								out_data = packetsList.get(nextSeqNum);
 							}
 							// else construct packet and add to list
-							else{
+							else {
 								// if first packet, special handling: prepend file information
-								if (nextSeqNum == 0){
-									byte[] fileNameBytes = fileName.getBytes();
-									byte[] fileNameLengthBytes = ByteBuffer.allocate(4).putInt(fileNameBytes.length).array();
+								if (nextSeqNum == 0) {
+									byte[] messageBytes = message.getBytes();
+									byte[] messageLengthBytes = ByteBuffer.allocate(4).putInt(message.length()).array();
+									
 									byte[] dataBuffer = new byte[data_size];
-									int dataLength = fis.read(dataBuffer, 0, data_size - 4 - fileNameBytes.length);
-									byte[] dataBytes = copyOfRange(dataBuffer, 0, dataLength);
-									ByteBuffer BB = ByteBuffer.allocate(4 + fileNameBytes.length + dataBytes.length);
-									BB.put(fileNameLengthBytes);	// file name length
-									BB.put(fileNameBytes);			// file name
-									BB.put(dataBytes);				// file data slice
+									ByteBuffer BB = ByteBuffer.allocate(4 + messageBytes.length);
+									BB.put(messageLengthBytes);
+									BB.put(messageBytes);
 									out_data = generatePacket(nextSeqNum, BB.array());
 								}
 								// else if subsequent packets
-								else{
-									byte[] dataBuffer = new byte[data_size];
-									int dataLength = fis.read(dataBuffer, 0, data_size);
-									// if no more data to be read, send empty data. i.e. finalSeqNum
-									if (dataLength == -1){
-										isFinalSeqNum = true;
-										out_data = generatePacket(nextSeqNum, new byte[0]);
-									}
-									// else if valid data
-									else{
-										byte[] dataBytes = copyOfRange(dataBuffer, 0, dataLength);
-										out_data = generatePacket(nextSeqNum, dataBytes);
-									}
+								else {
+									byte[] messageBytes = message.getBytes();
+									byte[] messageLengthBytes = ByteBuffer.allocate(4).putInt(message.length()).array();
+									out_data = generatePacket(nextSeqNum, messageBytes);
 								}
-								packetsList.add(out_data);	// add to packetsList
 							}
-							
+
 							// send the packet
 							sk_out.send(new DatagramPacket(out_data, out_data.length, dst_addr, dst_port));
 							System.out.println("Sender: Sent seqNum " + nextSeqNum);
-							
+
 							// update nextSeqNum if currently not at FinalSeqNum
-							if (!isFinalSeqNum) nextSeqNum++;
-							s.release();	/***** leave CS *****/
+							if (!isFinalSeqNum)
+								nextSeqNum++;
+							s.release(); /***** leave CS *****/
 						}
 						sleep(5);
 					}
 				} catch (Exception e) {
 					e.printStackTrace();
 				} finally {
-					setTimer(false);	// close timer
-					sk_out.close();		// close outgoing socket
-					fis.close();		// close FileInputStream
+					sk_out.close(); // close outgoing socket
+					// fis.close(); // close FileInputStream
 					System.out.println("Sender: sk_out closed!");
 				}
 			} catch (Exception e) {
@@ -146,59 +156,64 @@ public class Sender {
 				System.exit(-1);
 			}
 		}
-	}// END CLASS OutThread
+	}
 
-	// CLASS InThread
-	public class InThread extends Thread {
+
+	public class AcknowledgeThread extends Thread {
 		private DatagramSocket sk_in;
 
-		// InThread constructor
-		public InThread(DatagramSocket sk_in) {
+		public AcknowledgeThread(DatagramSocket sk_in) {
 			this.sk_in = sk_in;
 		}
 
 		// returns -1 if corrupted, else return Ack number
-		int decodePacket(byte[] pkt){
+		int decodePacket(byte[] pkt) {
 			byte[] received_checksumBytes = copyOfRange(pkt, 0, 8);
 			byte[] ackNumBytes = copyOfRange(pkt, 8, 12);
 			CRC32 checksum = new CRC32();
 			checksum.update(ackNumBytes);
-			byte[] calculated_checksumBytes = ByteBuffer.allocate(8).putLong(checksum.getValue()).array();// checksum (8 bytes)
-			if (Arrays.equals(received_checksumBytes, calculated_checksumBytes)) return ByteBuffer.wrap(ackNumBytes).getInt();
-			else return -1;
+			byte[] calculated_checksumBytes = ByteBuffer.allocate(8).putLong(checksum.getValue()).array();// checksum (8
+																											// bytes)
+			if (Arrays.equals(received_checksumBytes, calculated_checksumBytes))
+				return ByteBuffer.wrap(ackNumBytes).getInt();
+			else
+				return -1;
 		}
-		
+
 		// receiving process (updates base)
 		public void run() {
 			try {
-				byte[] in_data = new byte[12];	// ack packet with no data
-				DatagramPacket in_pkt = new DatagramPacket(in_data,	in_data.length);
+				byte[] in_data = new byte[12]; // ack packet with no data
+				DatagramPacket in_pkt = new DatagramPacket(in_data, in_data.length);
 				try {
 					// while there are still packets yet to be received by receiver
 					while (!isTransferComplete) {
-						
+
 						sk_in.receive(in_pkt);
 						int ackNum = decodePacket(in_data);
 						System.out.println("Sender: Received Ack " + ackNum);
-						
+
 						// if ack is not corrupted
-						if (ackNum != -1){
+						if (ackNum != -1) {
 							// if duplicate ack
-							if (base == ackNum + 1){
-								s.acquire();	/***** enter CS *****/
-								setTimer(false);		// off timer
-								nextSeqNum = base;		// resets nextSeqNum
-								s.release();	/***** leave CS *****/
+							if (base == ackNum + 1) {
+								s.acquire(); /***** enter CS *****/
+								nextSeqNum = base; // resets nextSeqNum
+								s.release(); /***** leave CS *****/
 							}
 							// else if teardown ack
-							else if (ackNum == -2) isTransferComplete = true;
+							else if (ackNum == 0)
+							// Transfer complete
+							{
+								isTransferComplete = true;
+								// send final ack
+							}
 							// else normal ack
-							else{
-								base = ackNum++;	// update base number
-								s.acquire();	/***** enter CS *****/
-								if (base == nextSeqNum) setTimer(false);	// if no more unacknowledged packets in pipe, off timer
-								else setTimer(true);						// else packet acknowledged, restart timer
-								s.release();	/***** leave CS *****/
+							else {
+								base = ackNum++; // update base number
+								s.acquire(); /***** enter CS *****/
+								// else packet acknowledged, restart timer
+								s.release(); /***** leave CS *****/
 							}
 						}
 						// else if ack corrupted, do nothing
@@ -214,66 +229,11 @@ public class Sender {
 				System.exit(-1);
 			}
 		}
-	}// END CLASS InThread
-
-
-	// Timeout task
-	public class Timeout extends TimerTask{
-		public void run(){
-			try{
-				s.acquire();	/***** enter CS *****/
-				System.out.println("Sender: Timeout!");
-				nextSeqNum = base;	// resets nextSeqNum
-				s.release();	/***** leave CS *****/
-			} catch(InterruptedException e){
-				e.printStackTrace();
-			}
-		}
-	}// END CLASS Timeout
-	
-	// sender constructor
-	public Sender(int sk1_dst_port, int sk4_dst_port, String path, String fileName) {
-		base = 0;
-		nextSeqNum = 0;
-		this.path = path;
-		this.fileName = fileName;
-		packetsList = new Vector<byte[]>(win_size);
-		isTransferComplete = false;
-		DatagramSocket sk1, sk4;
-		s = new Semaphore(1);
-		System.out.println("Sender: sk1_dst_port=" + sk1_dst_port + ", sk4_dst_port=" + sk4_dst_port + ", inputFilePath=" + path + ", outputFileName=" + fileName);
-		
-		try {
-			// create sockets
-			sk1 = new DatagramSocket();				// outgoing channel
-			sk4 = new DatagramSocket(sk4_dst_port);	// incoming channel
-
-			// create threads to process data
-			InThread th_in = new InThread(sk4);
-			OutThread th_out = new OutThread(sk1, sk1_dst_port, sk4_dst_port);
-			th_in.start();
-			th_out.start();
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-			System.exit(-1);
-		}
-	}// END Sender constructor
-
-	// same as Arrays.copyOfRange in 1.6
-	public byte[] copyOfRange(byte[] srcArr, int start, int end){
-		int length = (end > srcArr.length)? srcArr.length-start: end-start;
-		byte[] destArr = new byte[length];
-		System.arraycopy(srcArr, start, destArr, 0, length);
-		return destArr;
 	}
-	
+
 	public static void main(String[] args) {
-		// parse parameters
-		if (args.length != 4) {
-			System.err.println("Usage: java Sender sk1_dst_port, sk4_dst_port, inputFilePath, outputFileName");
-			System.exit(-1);
-		}
-		else new Sender(Integer.parseInt(args[0]), Integer.parseInt(args[1]), args[2], args[3]);
+		// sender port, receiver port, message
+		// ex: 4001 4002 "hello"
+		new Sender(4001, 4002, "Hello, receiver! how are you on this beautiful day?");
 	}
 }
